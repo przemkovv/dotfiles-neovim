0fed9094d3a84add6fd31dcbeb0f202c42c3b8c6
Przemysław Walkowiak <p.walkowiak@medicalgorithmics.com>
2025-08-07 14:20:48 +0200

    feat: add which-key

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -98,5 +98,23 @@ return {
       })
     end,
   },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        "<space>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  }
 
 }
