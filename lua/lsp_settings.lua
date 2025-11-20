61728b1c1954f509e8d68d85e8a847fc428b2824
Przemysław Walkowiak <p.walkowiak@medicalgorithmics.com>
2025-11-20 15:58:56 +0100

    feat: enable ts_ls

--- lua/lsp_settings.lua
+++ lua/lsp_settings.lua
@@ -43,6 +43,8 @@ vim.lsp.enable('powershell_es')
 vim.lsp.enable('copilot')
 vim.lsp.enable('rust_analyzer')
 vim.lsp.enable('dockerls')
+vim.lsp.enable('jqls')
+vim.lsp.enable('ts_ls')
 
 vim.lsp.handlers['textDocument/publishDiagnostics'] = require('lsp_utils').on_publish_diagnostics_with_related(vim.lsp
   .handlers['textDocument/publishDiagnostics'])
