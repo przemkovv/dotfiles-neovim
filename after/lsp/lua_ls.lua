e8780201e2c2f24c18f811ca89c506e34ef34848
Przemysław Walkowiak <przemkovv@gmail.com>
2025-12-13 03:45:48 +0100

    feat: move lua_ls config

--- after/lsp/lua_ls.lua
+++ after/lsp/lua_ls.lua
@@ -1,14 +1,47 @@
-local libraries = vim.list.unique(vim.iter({
-  -- vim.fn.expand('$VIMRUNTIME/lua'),
-  -- vim.fn.expand('$VIMRUNTIME/lua/vim/lsp'),
-  -- '${3rd}/luv/library',
-  -- '${3rd}/busted/library',
-  vim.api.nvim_get_runtime_file("lua", true),
-}):flatten():totable())
+local libraries =
+    vim.tbl_map(vim.fs.normalize,
+      vim.list.unique(vim.iter({
+        -- vim.fn.expand('$VIMRUNTIME/lua'),
+        -- vim.fn.expand('$VIMRUNTIME/lua/vim/lsp'),
+        -- '${3rd}/luv/library',
+        -- '${3rd}/busted/library',
+        vim.api.nvim_get_runtime_file("", true),
+      }):flatten():totable()))
+
+local config_dir = vim.fs.normalize(vim.fn.stdpath('config'))
+libraries = vim.tbl_filter(function(path)
+  return path ~= config_dir
+end, libraries)
 
 
 ---@type vim.lsp.Config
 return {
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if path ~= vim.fn.stdpath('config') and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then
+        return
+      end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        path = {
+          'lua/?.lua',
+          'lua/?/init.lua',
+        },
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = libraries,
+      }
+    })
+  end,
+
   settings = {
     Lua = {
       format = {
@@ -21,11 +54,6 @@ return {
           insert_final_newline = true,
         },
       },
-      workspace = {
-        checkThirdParty = false,
-        -- Make the server aware of Neovim runtime files
-        library = libraries,
-      },
       completion = {
         callSnippet = 'Replace',
       },
@@ -33,8 +61,6 @@ return {
       telemetry = {
         enable = false,
       },
-      codeLens = { enable = true },
-      hint = { enable = true, semicolon = 'Disable' },
     }
   }
 }
